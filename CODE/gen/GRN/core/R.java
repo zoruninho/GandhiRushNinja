3a631d37efa34199b21cb65a3c3d010571f1cc14
brotchain <alexpv14@gmail.com>
2014-03-20 10:47:17 +0100

    ajout image + musique

--- CODE/gen/GRN/core/R.java
+++ CODE/gen/GRN/core/R.java
@@ -24,6 +24,9 @@ public final class R {
         public static final int background=0x7f020000;
         public static final int ic_launcher=0x7f020001;
         public static final int ricepack=0x7f020002;
+        public static final int roue_param=0x7f020003;
+        public static final int vie_indien=0x7f020004;
+        public static final int vie_perdue=0x7f020005;
     }
     public static final class id {
         public static final int action_settings=0x7f090000;
